e2ba6217fce53c38ca31dabaa0c7a06c5d810367
Jon.Coello <Jon.Coello@WLKCND4071HPV>
2016-06-09 20:23:57 +0100

    Add project files.

---
+++ AxzureBlobExample/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace AxzureBlobExample {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.cmdUpload = new System.Windows.Forms.Button();
+            this.cmdDownload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdUpload
+            // 
+            this.cmdUpload.Location = new System.Drawing.Point(174, 182);
+            this.cmdUpload.Name = "cmdUpload";
+            this.cmdUpload.Size = new System.Drawing.Size(96, 23);
+            this.cmdUpload.TabIndex = 0;
+            this.cmdUpload.Text = "Upload";
+            this.cmdUpload.UseVisualStyleBackColor = true;
+            this.cmdUpload.Click += new System.EventHandler(this.cmdUpload_Click);
+            // 
+            // cmdDownload
+            // 
+            this.cmdDownload.Location = new System.Drawing.Point(174, 218);
+            this.cmdDownload.Name = "cmdDownload";
+            this.cmdDownload.Size = new System.Drawing.Size(96, 23);
+            this.cmdDownload.TabIndex = 1;
+            this.cmdDownload.Text = "Download";
+            this.cmdDownload.UseVisualStyleBackColor = true;
+            this.cmdDownload.Click += new System.EventHandler(this.cmdDownload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.cmdDownload);
+            this.Controls.Add(this.cmdUpload);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdUpload;
+        private System.Windows.Forms.Button cmdDownload;
+    }
+}
+
---
+++ AxzureBlobExample/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Blob; // Namespace for Blob storage types
+using System.IO;
+using System.Diagnostics;
+
+namespace AxzureBlobExample {
+    public partial class Form1 : Form {
+
+        private string _tag = null;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private async void cmdUpload_Click(object sender, EventArgs e) {
+
+            using (var ofd = new OpenFileDialog()) {
+
+                ofd.Filter = "PNG Files (*.png)|*.png";
+                ofd.ShowDialog();
+
+                CloudBlockBlob blockBlob = await GetBlockBlob();
+
+                using (var fs = File.OpenRead(ofd.FileName)) {
+
+                    var ac = 
+                        _tag == null ? 
+                        AccessCondition.GenerateIfNoneMatchCondition("*") : 
+                        AccessCondition.GenerateIfMatchCondition(_tag);
+
+                    await blockBlob.UploadFromStreamAsync(fs, ac, null, null);
+
+                }
+
+            }
+
+            MessageBox.Show("OK");
+
+        }
+
+        private async void cmdDownload_Click(object sender, EventArgs e) {
+
+            // Retrieve reference to a blob named "myblob".
+            CloudBlockBlob blockBlob = await GetBlockBlob();
+
+            using (var sfd = new SaveFileDialog()) {
+
+                sfd.Filter = "PNG Files (*.png)|*.png";
+                sfd.ShowDialog();
+
+                await blockBlob.DownloadToFileAsync(sfd.FileName, FileMode.Create);
+                _tag = blockBlob.Properties.ETag;
+
+                Process.Start(sfd.FileName);
+
+            }
+
+        }
+
+        private async Task<CloudBlockBlob> GetBlockBlob() {
+
+            // Retrieve storage account from connection string.
+            var setting = CloudConfigurationManager.GetSetting("StorageConnectionString");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(setting);
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve a reference to a container.
+            CloudBlobContainer container = blobClient.GetContainerReference("mycontainer");
+
+            // Create the container if it doesn't already exist.
+            await container.CreateIfNotExistsAsync();
+
+            // Retrieve reference to a blob named "myblob".
+            return container.GetBlockBlobReference("myblob");
+
+        }
+
+    }
+}
